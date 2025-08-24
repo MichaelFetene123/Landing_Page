b7ece83f309b839ea8dffcd37f41b5209b07f676
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-24 03:54:37 -0700

    Add Navbar component and asset files
    Introduced a Navbar component and updated App.jsx to render it. Added image and video assets including logo, code image, profile pictures, and videos to the assets directory for use in the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      {" "}
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
-}
+};
 
-export default App
+export default App;
