02507027890c908266fbf016eecf9e0823ccc3b7
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-25 07:47:28 -0700

    Add Testimonials component to App
    Introduces a new Testimonials component and integrates it into the main App layout. The component currently displays a section header for testimonials.

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react' 
+import {testimonials} from '../constants/index'
+
+const Testimonials = () => {
+  return (
+      <div className='mt-20  tracking-wide'>
+      <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center my-8'>Testimonials</h2>
+      </div>
+  )
+}
+
+export default Testimonials
