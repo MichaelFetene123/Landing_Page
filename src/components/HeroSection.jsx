35249f43c50022305e4f2d62f1053a86cb29fdfd
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-25 07:39:23 -0700

    Add hover effects to HeroSection buttons
    Enhanced the 'Start for free' and 'Documentation' buttons in HeroSection with scale and text color changes on hover for improved interactivity.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,8 +13,8 @@ const HeroSection = () => {
           </h1>
       <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>Empower your creativity and bring your VR app ideas to life with our intuitive development tools. Get started today and turn your imagination into immersive reality!</p>
       <div className="flex justify-center my-10">
-        <a href="#" className='bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md '>Start for free </a>
-        <a href="" className='py-3 px-4 mx-3 rounded-md border'>Documentation</a>
+        <a href="#" className='bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md hover:scale-105 hover:text-gray-100/80 '>Start for free </a>
+        <a href="" className='py-3 px-4 mx-3 rounded-md border hover:scale-105 hover:text-gray-100/80'>Documentation</a>
       </div>
       <div className='flex mt-10 justify-center ' >
         <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'> <source src={video1} type='video/mp4' />
