2fd20c566f8fe92ff1d9d03ad53bf497821357f4
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-25 07:29:10 -0700

    Add pricing details and features to Pricing component
    Displays the price per month and a list of features for each pricing option. Also adds a 'Subscribe' button for user interaction.

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -21,6 +21,19 @@ const Pricing = () => {
                   </span>
                 )}
               </p>
+              <p className="mb-8">
+                <span className="text-5xl mt-6 mr-2">{option.price}</span>
+                <span className="text-neutral-400 tracking-tight">/Month</span>
+              </p>
+              <ul>
+                {option.features.map((feature, index) => (
+                  <li key={index} className="mt-8 flex items-center">
+                    <CheckCircle2 className="mr-2 text-green-500" />
+                    <span className="ml-2">{feature}</span>
+                  </li>
+                ))}
+                    </ul>
+                    <a href="" className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-900 border border-orange-900 rounded-lg transition duration-200">Subscribe</a>
             </div>
           </div>
         ))}
